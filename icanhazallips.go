9c3bc782397a63f9e0626d9e6a3bcef3d11de56e
terminaldweller <thabogre@gmail.com>
2023-01-17 15:50:42 +0330

    update

--- icanhazallips.go
+++ icanhazallips.go
@@ -50,5 +50,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServeTLS(":8080", "/certs/server.cert", "/certs/server.key", nil))
 }
